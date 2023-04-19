4d9d726fffcdc556c4a5e040ae3a6e9cd9ff48b7
aidan.skomra <skomraa@sheridancollege.ca>
2023-04-19 09:21:06 -0400

    Completed up until part 2

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const dataService = require('./data-service');
 const express = require('express');
 const app = express();
 
-// Call the initialize() method from our data-service.js module
-dataService.initialize()
-  .then(() => {
-    // Start the server
-    app.listen(8080, () => {
-      console.log('Server listening on port 8080');
-    });
-  })
-  .catch((err) => {
-    // Output the error to the console
-    console.log(err);
-  });
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/views/about.html');
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Express http server listening on port ${port}`);
+});
